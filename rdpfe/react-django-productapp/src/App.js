ed7c77eb978b6384ab71d393e2a4abda50faf6dc
SumonRayy <sumonrayy@gmail.com>
2021-04-20 19:52:11 +0530

    Cart section created

--- rdpfe/react-django-productapp/src/App.js
+++ rdpfe/react-django-productapp/src/App.js
@@ -4,18 +4,44 @@ import Cart from './components/Cart';
 import Main from './components/Main';
 import NavBar from './components/NavBar';
 import data from './components/Data'
+import { useState } from 'react';
 
 function App() {
 
   const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id === product.id);
+    if(exist) {
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty+1 } : x))
+    }
+    else {
+      setCartItems([...cartItems,{...product, qty: 1}])
+    }
+  }
+
+  const onRemove = (product) => {
+    const exist = cartItems.find((x)=> x.id === product.id);
+    if(exist.qty===1) {
+      setCartItems(cartItems.filter((X)=> X.id !== product.id))
+    }else{
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty-1 } : x))
+    }
+  }
 
   return (
     <div className="App">
         <NavBar />
         <div className="card-container">
-          <Main products={products} />
+            <Main products={products} onAdd={onAdd}/>
           <div className="vertical-card-container">
-            <Cart />
+            <Cart 
+            cartItems={cartItems} 
+            setCartItems={setCartItems}
+            onAdd={onAdd} 
+            onRemove={onRemove} 
+            countCartItems={cartItems.length}/>
             <About />
           </div>
         </div>
--- rdpfe/react-django-productapp/src/components/Cart.js
+++ rdpfe/react-django-productapp/src/components/Cart.js
@@ -1,11 +1,73 @@
 import React from 'react'
 import cart from '../assets/imgs/cart.png'
 
-function Cart() {
+function Cart(props) {
+
+    const {cartItems, setCartItems, onAdd, onRemove, countCartItems} = props;
+
+    const itemsPrice = cartItems.reduce((a, c) => a+c.price * c.qty, 0);
+    const taxPrice = itemsPrice * 0.18;
+    const shippingPrice = itemsPrice > 5000 ? 0 : 50;
+    const totalPrice = itemsPrice + taxPrice + shippingPrice;
+
+    const checkoutHandler = () =>{
+        setCartItems([])
+        alert('Checkout Completed!')
+    }
+
     return (
         <div className="card">
-            <h2>Cart</h2>
-            <img src={cart} alt='cart-img'/>
+            <div className="cart-header">
+                <h2>Cart</h2>
+                <img src={cart} alt='cart-img'/>{' '}{countCartItems ? (
+                    <button className='badge'>{countCartItems}</button>
+                ) : (<></>)}
+            </div>
+            <div className="cart block">
+                {cartItems.length === 0 ? (<p>Cart is Empty</p>):(<div>
+                    {cartItems.map((item)=>(
+                        <div key={item.id} className="item">
+                            <div className="item-name">
+                                <h3>{item.title}</h3>
+                                <button onClick={() => onRemove(item)} className="remove">
+                                    -
+                                </button>{' '}
+                                <button onClick={() => onAdd(item)} className="add">
+                                    +
+                                </button>
+                            </div>
+                            <div className='qty'>
+                                {item.qty} x ₹ {item.price.toFixed(2)}
+                            </div>
+                        </div>
+                    ))}
+                    {cartItems.length !== 0 && (
+                        <>
+                        <hr></hr>
+                        <div className="row">
+                            <div className="col-2">Items Price</div>
+                            <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">Tax Price</div>
+                            <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2">Shipping Price</div>
+                            <div className="col-1 text-right">${shippingPrice.toFixed(2)}</div>
+                        </div>
+                        <div className="row">
+                            <div className="col-2"><strong>Total Price</strong></div>
+                            <div className="col-1 text-right"><strong>${totalPrice.toFixed(2)}</strong></div>
+                        </div>
+                        <hr/>
+                        <div className='rowcenter'>
+                            <button className="checkout" onClick={() => checkoutHandler()}>Checkout</button>
+                        </div>
+                        </>
+                    )}
+                </div>)}
+            </div>
         </div>
     )
 }
